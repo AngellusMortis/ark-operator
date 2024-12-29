221e9a6c4057c55d65b5b32c0311a4c5ce7d5829
Christopher Bailey <cbailey@mort.is>
2024-12-28 21:07:27 -0500

    Tweak tests

--- test/tests/ark/test_cluster.py
+++ test/tests/ark/test_cluster.py
@@ -113,7 +113,7 @@ async def test_update_cluster_create_with_data(k8s_namespace: str) -> None:
     spec.server.size = "20Mi"
     spec.data.size = "22Mi"
 
-    await asyncio.sleep(5)
+    await asyncio.sleep(10)
     await update_cluster(
         name="ark",
         namespace=k8s_namespace,
@@ -143,7 +143,7 @@ async def test_update_cluster_update(k8s_namespace: str) -> None:
     spec.server.size = "20Mi"
     spec.data.size = "22Mi"
 
-    await asyncio.sleep(5)
+    await asyncio.sleep(10)
 
     await update_cluster(name="ark", namespace=k8s_namespace, spec=spec)
 
