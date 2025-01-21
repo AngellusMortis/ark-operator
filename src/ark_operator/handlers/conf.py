437454bb09fb48aacb3d5b79bdb10bc9620f47b4
Christopher Bailey <cbailey@mort.is>
2025-01-21 17:49:01 -0500

    Prevent duplicate restarts

--- src/ark_operator/handlers/conf.py
+++ src/ark_operator/handlers/conf.py
@@ -60,6 +60,10 @@ async def on_update_conf(**kwargs: Unpack[ChangeEvent]) -> None:
             return
         raise
 
+    if not status.ready or not status.state or not status.state.startswith("Running"):
+        logger.info("Skipping conf update restart because cluster is not ready.")
+        return
+
     maps = [map_id] if map_id else cluster.server.all_maps
     logger.info("Restarting servers %s due to configuration update", maps)
     await restart_server_pods(
